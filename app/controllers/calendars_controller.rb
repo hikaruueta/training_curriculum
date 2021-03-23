26297727c1a742fb7856fffac52c4e6faff23c4d
hikaruueta <hkr.u.e.t.0821@gmail.com>
2021-03-23 19:31:26 +0900

    issue4 refix

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -8,14 +8,14 @@ class CalendarsController < ApplicationController
 
   # 予定の保存
   def create
-    Plan.create(calendars_params)
+    Plan.create(plan_params)
     redirect_to action: :index
   end
 
   private
 
-  def calendars_params
-    params.permit(:date, :plan)
+  def plan_params
+    params.require(:plan).permit(:date, :plan)
   end
 
   def get_week
